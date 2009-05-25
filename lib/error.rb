8847ba4886359e81831cfd14ee2c64355f7848d0
Vladimir Bobes Tuzinsky <vladimir@tuzinsky.com>
2009-05-25 08:29:16 +0200

    TextMagic::API::Error.new accepts code and message arguments in addition to a single response hash argument

--- lib/error.rb
+++ lib/error.rb
@@ -6,8 +6,12 @@ module TextMagic
 
       attr_reader :code, :message
 
-      def initialize(response)
-        @code, @message = response[:error_code], response[:error_message]
+      def initialize(*args)
+        if args.first.is_a?(Hash)
+          @code, @message = args.first['error_code'], args.first['error_message']
+        else
+          @code, @message = args
+        end
       end
 
       def to_s
