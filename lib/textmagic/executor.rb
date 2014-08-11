b08fb187729095e2f345b0e3d17d24693524e2a6
dp6ai <patrick.davenport@unboxedconsulting.com>
2014-08-11 11:15:16 +0100

    changed to use https base url

--- lib/textmagic/executor.rb
+++ lib/textmagic/executor.rb
@@ -1,11 +1,10 @@
 module TextMagic
-
   class API
 
     class Executor
 
       include HTTParty
-      base_uri "http://www.textmagic.com/app"
+      base_uri "https://www.textmagic.com/app"
 
       # Executes a command by sending a request to the TextMagic's Bulk
       # SMS gateway. This is a low-level generic method used by methods
